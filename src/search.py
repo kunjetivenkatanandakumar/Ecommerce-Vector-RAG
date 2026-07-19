fdcc6c3105b77a49d71462a47c482b3f50abed87
Kunjeti Venkata Nanda Kumar <63538807+kunjetivenkatanandakumar@users.noreply.github.com>
2026-07-19 16:44:32 +0530

    Update search.py

--- src/search.py
+++ src/search.py
@@ -1,6 +1,6 @@
 import os
-import chromadb
 import pandas as pd
+import chromadb
 from sentence_transformers import SentenceTransformer
 
 
@@ -8,35 +8,35 @@ class ProductSearchEngine:
 
     def __init__(self, db_path):
 
-        self.db_path = db_path
-
         self.client = chromadb.PersistentClient(path=db_path)
-
         self.model = SentenceTransformer("all-MiniLM-L6-v2")
 
         try:
             self.collection = self.client.get_collection("amazon_products")
-            print("Existing ChromaDB loaded.")
+            print("Loaded existing ChromaDB collection.")
 
         except Exception:
-
-            print("Collection not found.")
-            print("Creating ChromaDB...")
-
+            print("Collection not found. Creating a new one...")
             self.collection = self.build_database()
 
     def build_database(self):
 
-        data_path = os.path.join(
-            os.path.dirname(__file__),
-            "..",
-            "data",
-            "processed",
-            "amazon_products_embeddings.pkl"
+        data_path = os.path.abspath(
+            os.path.join(
+                os.path.dirname(__file__),
+                "..",
+                "data",
+                "processed",
+                "amazon_products_embeddings.pkl"
+            )
         )
 
-        df = pd.read_pickle(data_path)
+        print("Loading:", data_path)
 
+        if not os.path.exists(data_path):
+            raise FileNotFoundError(f"Embedding file not found: {data_path}")
+
+        df = pd.read_pickle(data_path)
         df = df.drop_duplicates(subset=["product_id"]).reset_index(drop=True)
 
         collection = self.client.create_collection("amazon_products")
@@ -56,28 +56,16 @@ class ProductSearchEngine:
             ]
         )
 
-        print("Database created successfully.")
-
+        print("Collection created successfully.")
         return collection
 
     def search(self, query, top_k=5):
 
-        embedding = self.model.encode(query).tolist()
+        query_embedding = self.model.encode(query).tolist()
 
         results = self.collection.query(
-            query_embeddings=[embedding],
+            query_embeddings=[query_embedding],
             n_results=top_k
         )
 
-        output = []
-
-        for metadata in results["metadatas"][0]:
-
-            output.append({
-                "product_name": metadata["product_name"],
-                "category": metadata["category"],
-                "price": metadata["price"],
-                "rating": metadata["rating"]
-            })
-
-        return output
+        return results["metadatas"][0]
